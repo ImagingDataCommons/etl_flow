150fbc578454ed0262cb52d9ac12a5fa871a9036
BillClifford <bcli4d>
2021-07-21 13:35:25 -0700

    Revised to account for newer DB table structure.

--- bq/gen_analysis_results_table/gen_analysis_results_metadata_table.v3.dev.py
+++ bq/gen_analysis_results_table/gen_analysis_results_metadata_table.v3.dev.py
@@ -16,6 +16,7 @@
 
 import argparse
 import sys
+from bq.gen_analysis_results_table.gen_analysis_results_metadata_table import gen_collections_table
 import os
 import json
 import time
@@ -28,57 +29,12 @@ from utilities.tcia_scrapers import scrape_tcia_analysis_collections_page
 
 # Return a list of the source_dois in IDC collections
 # This includes original collection and analysis results DOIs
-def get_all_idc_dois(client, args):
-    query = f"""
-         SELECT distinct se.source_doi as source_doi
-         FROM `{args.src_project}.{args.bqdataset_name}.{args.bq_collection_table}` as c
-         JOIN `{args.src_project}.{args.bqdataset_name}.{args.bq_patient_table}` as p
-         ON c.collection_id = p.collection_id
-         JOIN `{args.src_project}.{args.bqdataset_name}.{args.bq_study_table}` as st
-         ON p.submitter_case_id = st.submitter_case_id
-         JOIN `{args.src_project}.{args.bqdataset_name}.{args.bq_series_table}` as se
-         ON st.study_instance_uid = se.study_instance_uid
-         LEFT JOIN `{args.src_project}.{args.bqdataset_name}.{args.bq_excluded_collections}` as ex
-         ON LOWER(c.collection_id) = LOWER(ex.tcia_api_collection_id)
-         WHERE ex.tcia_api_collection_id IS NULL
-         """
-    result = client.query(query).result()
-    all_source_dois = [series['source_doi'] for series in result]
 
-    return all_source_dois
-
-def build_metadata(args, source_dois):
-    # Scrape the TCIA analysis results page for metadata
-    analysis_metadata = scrape_tcia_analysis_collections_page()
-
-    rows = []
-    for analysis_id, analysis_data in analysis_metadata.items():
-        # print(collection_id)
-        # if collection_data["DOI"].split('doi.org/')[1] in DOIs:
-        # If the DOI of this analysis result is in source_dois, then it is in the series table
-        # and therefore we have a series from this analysis result, and therefor we should include
-        # this analysis result in the analysis_results metadata table
-        if analysis_data["DOI"] in source_dois:
-            analysis_data["Collection"] = analysis_id
-            rows.append(json.dumps(analysis_data))
-    metadata = '\n'.join(rows)
-    return metadata
-
-def gen_collections_table(args):
-    BQ_client = bigquery.Client(project=args.src_project)
-    source_dois = get_all_idc_dois(BQ_client, args)
-
-    metadata = build_metadata(args, source_dois)
-    job = load_BQ_from_json(BQ_client, args.dst_project, args.bqdataset_name, args.bqtable_name, metadata, analysis_results_metadata_schema)
-    while not job.state == 'DONE':
-        print('Status: {}'.format(job.state))
-        time.sleep(args.period * 60)
-    print("{}: Completed collections metatdata upload \n".format(time.asctime()))
 
 if __name__ == '__main__':
     parser =argparse.ArgumentParser()
 
-    parser.add_argument('--version', default=1, help='IDC version for which to build the table')
+    parser.add_argument('--version', default=3, help='IDC version for which to build the table')
     args = parser.parse_args()
     parser.add_argument('--src_project', default='idc-dev-etl')
     parser.add_argument('--dst_project', default='idc-dev-etl')
