235a490b596b57af0eb69ee2c4cca4663c1182a9
Andrey Fedorov <andrey.fedorov@gmail.com>
2023-01-20 16:17:50 -0500

    update dicom_metadata_curated_series_level
    * remove ROUND that was included by mistake for PixelSpacing
    * rename max_Rows/max_Columns

--- bq/derived_table_creation/BQ_Table_Building/derived_data_views/sql/dicom_metadata_curated_series_level.sql
+++ bq/derived_table_creation/BQ_Table_Building/derived_data_views/sql/dicom_metadata_curated_series_level.sql
@@ -9,25 +9,25 @@ WITH
     MAX(DISTINCT(TotalPixelMatrixRows)) AS max_TotalPixelMatrixRows,
     MAX(DISTINCT(`Columns`)) AS max_Columns,
     MAX(DISTINCT(`Rows`)) AS max_Rows,
-    MIN(DISTINCT(ROUND(SAFE_CAST(PixelSpacing[SAFE_OFFSET(0)] AS FLOAT64), 2))) AS min_spacing_0,
-    MIN(DISTINCT(ROUND(SAFE_CAST(PixelSpacing[SAFE_OFFSET(1)] AS FLOAT64), 2))) AS min_spacing_1,
+    MIN(DISTINCT(SAFE_CAST(PixelSpacing[SAFE_OFFSET(0)] AS FLOAT64))) AS min_spacing_0,
+    MIN(DISTINCT(SAFE_CAST(PixelSpacing[SAFE_OFFSET(1)] AS FLOAT64))) AS min_spacing_1,
     MIN(SAFE_CAST(SharedFunctionalGroupsSequence[SAFE_OFFSET(0)].PixelMeasuresSequence[SAFE_OFFSET(0)]. PixelSpacing[SAFE_OFFSET(0)] AS FLOAT64)) AS fg_min_spacing_0,
     MIN(SAFE_CAST(SharedFunctionalGroupsSequence[SAFE_OFFSET(0)].PixelMeasuresSequence[SAFE_OFFSET(0)]. PixelSpacing[SAFE_OFFSET(1)] AS FLOAT64)) AS fg_min_spacing_1,
     ARRAY_AGG(DISTINCT(CONCAT(SpecimenDescriptionSequence[SAFE_OFFSET(0)].PrimaryAnatomicStructureSequence[SAFE_OFFSET(0)].CodeValue,",", SpecimenDescriptionSequence[SAFE_OFFSET(0)].PrimaryAnatomicStructureSequence[SAFE_OFFSET(0)].CodingSchemeDesignator,",", SpecimenDescriptionSequence[SAFE_OFFSET(0)].PrimaryAnatomicStructureSequence[SAFE_OFFSET(0)].CodeMeaning )) IGNORE NULLS) AS primaryAnatomicStructure,
     ARRAY_AGG(DISTINCT(CONCAT(OpticalPathSequence[SAFE_OFFSET(0)].IlluminationTypeCodeSequence[SAFE_OFFSET(0)].CodeValue,",", OpticalPathSequence[SAFE_OFFSET(0)].IlluminationTypeCodeSequence[SAFE_OFFSET(0)].CodingSchemeDesignator,",", OpticalPathSequence[SAFE_OFFSET(0)].IlluminationTypeCodeSequence[SAFE_OFFSET(0)].CodeMeaning )) IGNORE NULLS) AS illuminationType,
   FROM
-    `bigquery-public-data.idc_current.dicom_all` AS dicom_all
+    `idc-dev-etl.idc_v13_pub.dicom_all` AS dicom_all
   GROUP BY
     SeriesInstanceUID
   )
 SELECT
   SeriesInstanceUID,
   COALESCE(min_spacing_0, fg_min_spacing_0) AS min_PixelSpacing0,
-  COALESCE(max_TotalPixelMatrixColumns, max_Columns) AS max_Columns,
-  COALESCE(max_TotalPixelMatrixRows, max_Rows) AS max_Rows,
+  COALESCE(max_TotalPixelMatrixColumns, max_Columns) AS max_TotalPixelMatrixColumns,
+  COALESCE(max_TotalPixelMatrixRows, max_Rows) AS max_TotalPixelMatrixRows,
   ObjectiveLensPower,
   primaryAnatomicStructure,
   illuminationType,
-  #if(Modality = "SM", slim_viewer_url, ohif_viewer_url) as viewer_url
+  Modality,
 FROM
   temp_table
