a278993639a1c7304e4b5a7810807d34eaa3eaff
BillClifford <bcli4d>
2023-01-27 08:57:17 -0800

    Added 2sf rounding to min_pixelspacing0

--- bq/derived_table_creation/BQ_Table_Building/derived_data_views/sql/dicom_metadata_curated_series_level.sql
+++ bq/derived_table_creation/BQ_Table_Building/derived_data_views/sql/dicom_metadata_curated_series_level.sql
@@ -10,19 +10,18 @@ WITH
     MAX(DISTINCT(`Columns`)) AS max_Columns,
     MAX(DISTINCT(`Rows`)) AS max_Rows,
     MIN(DISTINCT(SAFE_CAST(PixelSpacing[SAFE_OFFSET(0)] AS FLOAT64))) AS min_spacing_0,
-    MIN(DISTINCT(SAFE_CAST(PixelSpacing[SAFE_OFFSET(1)] AS FLOAT64))) AS min_spacing_1,
     MIN(SAFE_CAST(SharedFunctionalGroupsSequence[SAFE_OFFSET(0)].PixelMeasuresSequence[SAFE_OFFSET(0)]. PixelSpacing[SAFE_OFFSET(0)] AS FLOAT64)) AS fg_min_spacing_0,
-    MIN(SAFE_CAST(SharedFunctionalGroupsSequence[SAFE_OFFSET(0)].PixelMeasuresSequence[SAFE_OFFSET(0)]. PixelSpacing[SAFE_OFFSET(1)] AS FLOAT64)) AS fg_min_spacing_1,
     ARRAY_AGG(DISTINCT(CONCAT(SpecimenDescriptionSequence[SAFE_OFFSET(0)].PrimaryAnatomicStructureSequence[SAFE_OFFSET(0)].CodingSchemeDesignator,":", SpecimenDescriptionSequence[SAFE_OFFSET(0)].PrimaryAnatomicStructureSequence[SAFE_OFFSET(0)].CodeValue, ":", SpecimenDescriptionSequence[SAFE_OFFSET(0)].PrimaryAnatomicStructureSequence[SAFE_OFFSET(0)].CodeMeaning)) IGNORE NULLS)[SAFE_OFFSET(0)] AS primaryAnatomicStructure_code_str,
     ARRAY_AGG(DISTINCT(CONCAT(OpticalPathSequence[SAFE_OFFSET(0)].IlluminationTypeCodeSequence[SAFE_OFFSET(0)].CodingSchemeDesignator,":", OpticalPathSequence[SAFE_OFFSET(0)].IlluminationTypeCodeSequence[SAFE_OFFSET(0)].CodeValue, ":", OpticalPathSequence[SAFE_OFFSET(0)].IlluminationTypeCodeSequence[SAFE_OFFSET(0)].CodeMeaning)) IGNORE NULLS)[SAFE_OFFSET(0)] AS illuminationType_code_str,
   FROM
-    `idc-dev-etl.idc_v13_pub.dicom_all` AS dicom_all
+    `{project}.{dataset}.dicom_all` AS dicom_all
   GROUP BY
     SeriesInstanceUID
   )
 SELECT
   SeriesInstanceUID,
-  COALESCE(min_spacing_0, fg_min_spacing_0) AS min_PixelSpacing0,
+  if(COALESCE(min_spacing_0, fg_min_spacing_0) = 0, 0,
+    round(COALESCE(min_spacing_0, fg_min_spacing_0) ,CAST(2 -1-floor(log10(abs(COALESCE(min_spacing_0, fg_min_spacing_0) ))) AS INT64))) AS min_PixelSpacing_2sf,
   COALESCE(max_TotalPixelMatrixColumns, max_Columns) AS max_TotalPixelMatrixColumns,
   COALESCE(max_TotalPixelMatrixRows, max_Rows) AS max_TotalPixelMatrixRows,
   SAFE_CAST(ObjectiveLensPower as INT) as ObjectiveLensPower,
