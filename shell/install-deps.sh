0ba6b73485f20843218072edd1e20710cfd490ee
BillClifford <bcli4d>
2024-03-20 08:59:16 -0700

    Update orb version

--- shell/install-deps.sh
+++ shell/install-deps.sh
@@ -62,10 +62,11 @@
 
 # Install our primary python libraries
 # If we're not on CircleCI, or we are but the lib directory isn't there (cache miss), install lib
-if [ -z "${CI}" ] || [ ! -d "lib" ]; then
+#if [ -z "${CI}" ] || [ ! -d "lib" ]; then
+if [ -z "${CI}" ] || [ -z $(pip list| grep pydicom) ]; then
     echo "Installing Python Libraries..."
-    pip3 install -r $PWD/requirements.txt -t $HOME/lib --upgrade --only-binary all
-#    pip3 install -r $PWD/requirements.txt --upgrade --only-binary all
+#    pip3 install -r $PWD/requirements.txt -t $HOME/lib --upgrade --only-binary all
+    pip3 install -r $PWD/requirements.txt --upgrade --only-binary all
 else
     echo "Using restored cache for Python Libraries"
 fi
