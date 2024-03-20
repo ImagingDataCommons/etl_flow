745bc9e19c020e2df4553546a044b6f69add3dbe
BillClifford <bcli4d>
2024-03-20 10:24:51 -0700

    Update orb version

--- shell/install-deps.sh
+++ shell/install-deps.sh
@@ -2,7 +2,7 @@
 
 # Install our primary python libraries
 # If we're not on CircleCI, or we are but the lib directory isn't there (cache miss), install lib
-if [ -z "${CI}" ] || [ ! -d "lib" ]; then
+if [ -z "${CI}" ] || [ ! -d "${HOME}/lib" ]; then
     echo "Installing Python Libraries..."
     pip3 install -r $PWD/requirements.txt -t $HOME/lib --upgrade --only-binary all
 #    pip3 install -r $PWD/requirements.txt --upgrade --only-binary all
