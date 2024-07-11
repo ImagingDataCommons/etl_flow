d04d7ac320892e59e4f4a69d38beff41ec64ef0f
Andrey Fedorov <andrey.fedorov@gmail.com>
2024-07-11 12:27:54 -0400

    added embedding, tissue fixative and staining codes

--- bq/generate_tables_and_views/derived_tables/BQ_Table_Building/derived_data_views/sql/dicom_metadata_curated_series_level.sql
+++ bq/generate_tables_and_views/derived_tables/BQ_Table_Building/derived_data_views/sql/dicom_metadata_curated_series_level.sql
@@ -13,14 +13,94 @@ WITH
     MIN(SAFE_CAST(SharedFunctionalGroupsSequence[SAFE_OFFSET(0)].PixelMeasuresSequence[SAFE_OFFSET(0)]. PixelSpacing[SAFE_OFFSET(0)] AS FLOAT64)) AS fg_min_spacing_0,
     ARRAY_AGG(DISTINCT(CONCAT(SpecimenDescriptionSequence[SAFE_OFFSET(0)].PrimaryAnatomicStructureSequence[SAFE_OFFSET(0)].CodingSchemeDesignator,":", SpecimenDescriptionSequence[SAFE_OFFSET(0)].PrimaryAnatomicStructureSequence[SAFE_OFFSET(0)].CodeValue, ":", SpecimenDescriptionSequence[SAFE_OFFSET(0)].PrimaryAnatomicStructureSequence[SAFE_OFFSET(0)].CodeMeaning)) IGNORE NULLS)[SAFE_OFFSET(0)] AS primaryAnatomicStructure_code_str,
     ARRAY_AGG(DISTINCT(CONCAT(SpecimenDescriptionSequence[SAFE_OFFSET(0)].PrimaryAnatomicStructureSequence[SAFE_OFFSET(0)].PrimaryAnatomicStructureModifierSequence[SAFE_OFFSET(0)].CodingSchemeDesignator,":", SpecimenDescriptionSequence[SAFE_OFFSET(0)].PrimaryAnatomicStructureSequence[SAFE_OFFSET(0)].PrimaryAnatomicStructureModifierSequence[SAFE_OFFSET(0)].CodeValue, ":", SpecimenDescriptionSequence[SAFE_OFFSET(0)].PrimaryAnatomicStructureSequence[SAFE_OFFSET(0)].PrimaryAnatomicStructureModifierSequence[SAFE_OFFSET(0)].CodeMeaning)) IGNORE NULLS)[SAFE_OFFSET(0)] AS primaryAnatomicStructureModifier_code_str,
+
     ARRAY_AGG(DISTINCT(CONCAT(OpticalPathSequence[SAFE_OFFSET(0)].IlluminationTypeCodeSequence[SAFE_OFFSET(0)].CodingSchemeDesignator,":", OpticalPathSequence[SAFE_OFFSET(0)].IlluminationTypeCodeSequence[SAFE_OFFSET(0)].CodeValue, ":", OpticalPathSequence[SAFE_OFFSET(0)].IlluminationTypeCodeSequence[SAFE_OFFSET(0)].CodeMeaning)) IGNORE NULLS)[SAFE_OFFSET(0)] AS illuminationType_code_str,
   FROM
     `{project}.{dataset}.dicom_all` AS dicom_all
   GROUP BY
     SeriesInstanceUID
-  )
+  ),
+
+SpecimenPreparationSequence_unnested AS (
+      SELECT
+        SeriesInstanceUID,
+        concept_name_code_sequence.CodeMeaning AS cnc_cm,
+        concept_name_code_sequence.CodingSchemeDesignator AS cnc_csd,
+        concept_name_code_sequence.CodeValue AS cnc_val,
+        concept_code_sequence.CodeMeaning AS ccs_cm,
+        concept_code_sequence.CodingSchemeDesignator AS ccs_csd,
+        concept_code_sequence.CodeValue AS ccs_val,
+      FROM `bigquery-public-data.idc_v18.dicom_all`,
+      UNNEST(SpecimenDescriptionSequence[SAFE_OFFSET(0)].SpecimenPreparationSequence) as preparation_unnest_step1,
+      UNNEST(preparation_unnest_step1.SpecimenPreparationStepContentItemSequence) as preparation_unnest_step2,
+      UNNEST(preparation_unnest_step2.ConceptNameCodeSequence) as concept_name_code_sequence,
+      UNNEST(preparation_unnest_step2.ConceptCodeSequence) as concept_code_sequence
+    ),
+
+    slide_embedding AS (
+    SELECT
+      SeriesInstanceUID,
+      ARRAY_AGG(DISTINCT(CONCAT(ccs_cm,":",ccs_csd,":",ccs_val))) as embeddingMedium_code_str
+    FROM SpecimenPreparationSequence_unnested
+    WHERE (cnc_csd = 'SCT' and cnc_val = '430863003') -- CodeMeaning is 'Embedding medium'
+    GROUP BY SeriesInstanceUID
+    ),
+
+    slide_fixative AS (
+    SELECT
+      SeriesInstanceUID,
+      ARRAY_AGG(DISTINCT(CONCAT(ccs_cm, ":", ccs_csd,":",ccs_val))) as tissueFixative_code_str
+    FROM SpecimenPreparationSequence_unnested
+    WHERE (cnc_csd = 'SCT' and cnc_val = '430864009') -- CodeMeaning is 'Tissue Fixative'
+    GROUP BY SeriesInstanceUID
+    ),
+
+    slide_staining AS (
+    SELECT
+      SeriesInstanceUID,
+      ARRAY_AGG(DISTINCT(CONCAT(ccs_cm, ":", ccs_csd,":",ccs_val))) as staining_usingSubstance_code_str,
+    FROM SpecimenPreparationSequence_unnested
+    WHERE (cnc_csd = 'SCT' and cnc_val = '424361007') -- CodeMeaning is 'Using substance'
+    GROUP BY SeriesInstanceUID
+    )
+
 SELECT
-  SeriesInstanceUID,
+  temp_table.SeriesInstanceUID,
+  temp_table.Modality,
+  -- Embedding Medium
+  ARRAY(
+    SELECT IF(code IS NULL, NULL, SPLIT(code, ':')[SAFE_OFFSET(0)])
+    FROM UNNEST(embeddingMedium_code_str) AS code
+  ) AS embeddingMedium_CodeMeaning,
+  ARRAY(
+    SELECT IF(code IS NULL, NULL,
+              IF(STRPOS(code, ':') = 0, NULL,
+                 SUBSTR(code, STRPOS(code, ':') + 1)))
+    FROM UNNEST(embeddingMedium_code_str) AS code
+  ) AS embeddingMedium_code_designator_value_str,
+  -- Tissue Fixative
+  ARRAY(
+    SELECT IF(code IS NULL, NULL, SPLIT(code, ':')[SAFE_OFFSET(0)])
+    FROM UNNEST(tissueFixative_code_str) AS code
+  ) AS tissueFixative_CodeMeaning,
+  ARRAY(
+    SELECT IF(code IS NULL, NULL,
+              IF(STRPOS(code, ':') = 0, NULL,
+                 SUBSTR(code, STRPOS(code, ':') + 1)))
+    FROM UNNEST(tissueFixative_code_str) AS code
+  ) AS tissueFixative_code_designator_value_str,
+  -- Staining using substance
+  ARRAY(
+    SELECT IF(code IS NULL, NULL, SPLIT(code, ':')[SAFE_OFFSET(0)])
+    FROM UNNEST(staining_usingSubstance_code_str) AS code
+  ) AS staining_usingSubstance_CodeMeaning,
+  ARRAY(
+    SELECT IF(code IS NULL, NULL,
+              IF(STRPOS(code, ':') = 0, NULL,
+                 SUBSTR(code, STRPOS(code, ':') + 1)))
+    FROM UNNEST(staining_usingSubstance_code_str) AS code
+  ) AS staining_usingSubstance_code_designator_value_str,
+
   if(COALESCE(min_spacing_0, fg_min_spacing_0) = 0, 0,
     round(COALESCE(min_spacing_0, fg_min_spacing_0) ,CAST(2 -1-floor(log10(abs(COALESCE(min_spacing_0, fg_min_spacing_0) ))) AS INT64))) AS min_PixelSpacing_2sf,
   COALESCE(max_TotalPixelMatrixColumns, max_Columns) AS max_TotalPixelMatrixColumns,
@@ -30,8 +110,11 @@ SELECT
   SPLIT(primaryAnatomicStructure_code_str,":")[SAFE_OFFSET(2)] as primaryAnatomicStructure_CodeMeaning,
   CONCAT(SPLIT(primaryAnatomicStructureModifier_code_str,":")[SAFE_OFFSET(0)],":",SPLIT(primaryAnatomicStructureModifier_code_str,":")[SAFE_OFFSET(1)]) as primaryAnatomicStructureModifier_code_designator_value_str,
   SPLIT(primaryAnatomicStructureModifier_code_str,":")[SAFE_OFFSET(2)] as primaryAnatomicStructureModifier_CodeMeaning,
+
   CONCAT(SPLIT(illuminationType_code_str,":")[SAFE_OFFSET(0)],":",SPLIT(illuminationType_code_str,":")[SAFE_OFFSET(1)]) as illuminationType_code_designator_value_str,
   SPLIT(illuminationType_code_str,":")[SAFE_OFFSET(2)] as illuminationType_CodeMeaning,
-  Modality,
 FROM
   temp_table
+LEFT JOIN slide_embedding on temp_table.SeriesInstanceUID = slide_embedding.SeriesInstanceUID
+LEFT JOIN slide_fixative on temp_table.SeriesInstanceUID = slide_fixative.SeriesInstanceUID
+LEFT JOIN slide_staining on temp_table.SeriesInstanceUID = slide_staining.SeriesInstanceUID
